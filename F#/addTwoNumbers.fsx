680fbca5e9ecf9e36196a97c2493faac95ea86d4
C-Vilen <101933465+C-Vilen@users.noreply.github.com>
2023-03-09 11:27:40 +0100

    Rewrite F# task to enable testing

--- F#/addTwoNumbers.fsx
+++ F#/addTwoNumbers.fsx
@@ -1,10 +1,12 @@
 open System
 
+let addTwoNumbers a b = a + b
+
 [<EntryPoint>]
 let main argv =
-    let inputs = Console.ReadLine().Split(' ')
+    let inputs = System.Console.ReadLine().Split(' ')
     let a = int inputs.[0]
     let b = int inputs.[1]
-    let sum = a + b
+    let sum = addTwoNumbers a b
     printfn "%d" sum
     0
